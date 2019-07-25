0ad48cba3edbdb2fdedde8406affe8cefc164f27
Zhylkaaa <zhylko.dima@gmail.com>
2019-07-25 15:08:24 +0200

    tests

---
+++ tests/test_create_tags_wrap.py
@@ -0,0 +1,26 @@
+import time
+from krnnt_utils import create_tags4_without_guesser
+
+inp = []
+total_time = 0.0
+
+f = open('features.txt')
+lines = f.readlines()
+p = open('out2.txt', 'w+')
+
+for i in lines:
+    if(i != "EOW\n"):
+        inp.append(i[:-1])
+    else:
+        start = time.time()
+        res = create_tags4_without_guesser(inp)
+        end = time.time()
+        total_time += (end-start)
+        inp = []
+        for j in res:
+            p.write(j)
+            p.write('\n')
+f.close()
+p.close()
+
+print(total_time)

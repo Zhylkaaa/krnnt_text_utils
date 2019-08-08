54d85e49ac39832da7e2bc69e58a51b9ef930785
dimazhylko <zhylko.dima@gmail.com>
2019-08-08 11:23:21 +0200

    update setup

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(
-    ext_modules = cythonize(Extension("krnnt_utils", sources=["krnnt_utils.pyx"], include_dirs=[np.get_include()]), annotate=True)
+	name="krnnt_utils",
+	version="1.0.0",
+    ext_modules = cythonize(Extension("krnnt_utils", sources=["krnnt_utils.pyx"], include_dirs=[np.get_include()]), annotate=True),
+    install_requires=["Cython"]
 )
